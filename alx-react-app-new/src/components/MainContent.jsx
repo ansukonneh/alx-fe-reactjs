bc33d7ad28eb9163fd38bc2378897e1920742511
Ansu Konneh <ansukonneh028@gmail.com>
2025-11-26 21:50:47 +0000

    added a new folder

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <main
+      style={{
+        padding: '20px',
+        backgroundColor: '#eef2ff',
+        fontSize: '18px'
+      }}
+    >
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
