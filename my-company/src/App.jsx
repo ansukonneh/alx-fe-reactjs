c79398ec2a94b832dac54a2a45092d7266fce3ad
Ansu Konneh <ansukonneh028@gmail.com>
2025-11-26 22:48:01 +0000

    fixed a bug

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
-import Contact from './pages/Contact';
+import Contact from './pages/contact';
+
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </Router>
   );
