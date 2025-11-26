59cf12b26bb6fbb2f7f510ac08c827e4fa269971
Ansu Konneh <ansukonneh028@gmail.com>
2025-11-26 21:25:45 +0000

    added a new component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -7,12 +7,18 @@ import './App.css'
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 
+import UserProfile from './components/UserProfile'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+         <UserProfile 
+        name="Ansu" 
+        age="18" 
+        bio="Loves coding" />
         <Header />
       <MainContent />
       <Footer />
