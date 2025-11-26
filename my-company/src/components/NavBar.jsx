8692ba96764fe14ea5a456ec281f3db3f424f444
Ansu Konneh <ansukonneh028@gmail.com>
2025-11-26 23:45:22 +0000

    added a new file

--- my-company/src/components/NavBar.jsx
+++ my-company/src/components/NavBar.jsx
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-function NavBar() {
+function Navbar() {
   return (
     <nav style={{ padding: '10px', backgroundColor: '#333', color: '#fff' }}>
-      <Link to="/" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ margin: '0 15px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
+      <Link to="/" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px', color: '#fff', textDecoration: 'none' }}>Contact</Link>
     </nav>
   );
 }
 
-export default NavBar;
+export default Navbar;
