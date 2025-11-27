7e194e73c51b1510745c780fbc088d2211e3bc7a
Ansu Konneh <ansukonneh028@gmail.com>
2025-11-27 08:32:38 +0000

    added a new folder

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/NavBar.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Services from './Services.jsx'
+import Contact from './Contact.jsx'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  )
+}
+
+export default App
