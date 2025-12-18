697113c5e350c2f9cccd4ad49779540fe5a9a070
Ansu Konneh <ansukonneh028@gmail.com>
2025-12-18 02:47:54 +0000

    APP: fixed a bug

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import HomePage from "./components/HomePage"
 
@@ -9,4 +7,5 @@ function App() {
 }
 
 export default App
+
 
